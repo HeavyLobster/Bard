e1ffa7246d3de306237c5d69d3fc0364f5725378
HeavyLobster <heavylobster@users.noreply.github.com>
2017-03-24 07:31:53 -0700

    Fix handling edited messages while Volcyy's bot is online

--- src/bot.py
+++ src/bot.py
@@ -9,35 +9,21 @@ print('Loading Bot... ', end='')
 client = discord.Client()
 
 
-# Declear boolean to keep track of whether Volcyy's bot is online
+# Declare boolean to keep track of whether Volcyy's bot is online
+global volcyyBotOnline
 volcyyBotOnline = False
-
-
-# To avoid conflicts with another Bard bot, ignore commands and
-# change status to make it clearer what's going on.
-
-def becomeInactive():
-
-    global volcyyBotOnline
-    volcyyBotOnline = True
-
-
-# Same as above, but becoming active instead of inactive
-
-def becomeActive():
-
-    global volcyyBotOnline
-    volcyyBotOnline = False
 
 
 @client.event
 async def on_ready():
 
+    global volcyyBotOnline
+
     # Detect Volcyy's bot when starting
     volcyyBot = client.get_guild(172226206375084032).get_member(226612862620008448)
 
     if volcyyBot.status != discord.Status.offline:
-        becomeInactive()
+        volcyyBotOnline = True
         await client.change_presence(game=discord.Game(name="hide and seek with Bard"), status=discord.Status.dnd, afk=False)
 
     await ready.on_ready()
@@ -51,7 +37,9 @@ async def on_resumed():
 @client.event
 async def on_message(msg):
 
-    # Check if Volcyy's bot is online:
+    global volcyyBotOnline
+
+    # Check if Volcyy's bot is online
     if not volcyyBotOnline:
 
         try:
@@ -68,15 +56,21 @@ async def on_message_delete(msg):
 
 @client.event
 async def on_message_edit(before, after):
-    await message.handle_message(after)
+
+    global volcyyBotOnline
+
+    # Check if Volcyy's bot is online:
+    if not volcyyBotOnline:
+
+        await message.handle_message(after)
 
 
 @client.event
 async def on_reaction_add(reaction, user):
 
-# Check if Volcyy's bot is online
-
     global volcyyBotOnline
+
+    # Check if Volcyy's bot is online
     if not volcyyBotOnline:
 
         # Actual function
@@ -119,6 +113,8 @@ async def on_channel_update(before, after):
 @client.event
 async def on_member_join(member):
 
+    global volcyyBotOnline
+
     # Check if Volcyy's bot is online
     if not volcyyBotOnline:
         await members.join(member)
@@ -160,12 +156,12 @@ async def on_member_update(before, after):
 
         # If offline and coming online
         if before.status == discord.Status.offline and after.status != discord.Status.offline:
-            becomeInactive()
+            volcyyBotOnline = True
             await client.change_presence(game=discord.Game(name="hide and seek with Bard"), status=discord.Status.dnd, afk=False)
 
         # If online and going offline
         if before.status != discord.Status.offline and after.status == discord.Status.offline:
-            becomeActive()
+            volcyyBotOnline = False
             await client.change_presence(status=discord.Status.online)
 
 
