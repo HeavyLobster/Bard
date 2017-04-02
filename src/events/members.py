0e046740367cf075480be39aef6c6b0048c8e03f
HeavyLobster <heavylobster@users.noreply.github.com>
2017-04-02 16:09:46 -0700

    Fix role-assignment channel link

--- src/events/members.py
+++ src/events/members.py
@@ -4,8 +4,9 @@ print('Loading Member Event Handler... ', end='')
 
 
 async def join(member):
+
     await member.guild.default_channel.send(f'Welcome {member.mention} to the {member.guild.name}! '
-                                            'Head to role-assignment to get Roles! <:bardlove:242942446072233984>')
+                                            'Head to <#265551115901206528> to get Roles! <:bardlove:242942446072233984>')
 
 
 async def leave(member):
