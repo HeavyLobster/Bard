592496cd14b6b63873db64d6a83547d5dacf585a
HeavyLobster <heavylobster@users.noreply.github.com>
2017-04-17 23:09:19 -0700

    Add chance for chwanni greeting

--- src/events/members.py
+++ src/events/members.py
@@ -1,3 +1,5 @@
+import random
+
 # Member Join, Member Remove, Member Update, Member Ban, Member Unban
 
 print('Loading Member Event Handler... ', end='')
@@ -5,8 +7,11 @@ print('Loading Member Event Handler... ', end='')
 
 async def join(member):
 
-    await member.guild.default_channel.send(f'Welcome {member.mention} to the {member.guild.name}! '
+    if random.randrange(6) != 0:
+        await member.guild.default_channel.send(f'Welcome {member.mention} to the {member.guild.name}! '
                                             'Head to <#265551115901206528> to get Roles! <:bardlove:242942446072233984>')
+    else:
+        await member.guild.default_channel.send(f'Welcome {member.mention} ! <:bardhi2:269858268048916480> Have some <:cacao:269857893086527488> and <:porosnax:278951733609234433> <:bardhug:269858053820645389> \nHead to <#265551115901206528> to get Roles! <:bardlove:242942446072233984>')
 
 
 async def leave(member):
